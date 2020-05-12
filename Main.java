c6806cffcae212a6d4430f0af3ff98024dde7738
Viktys <vik_sokolova@hotmail.com>
2020-05-12 21:53:21 +0200

    lesson1

---
+++ Main.java
@@ -0,0 +1,70 @@
+package com.company;
+
+public class Main {
+
+    //задание 1
+    public static void main(String[] args) {
+
+
+        //задание 2
+        int num = 1;
+        long longNum = 2;
+        short shortNum = 3;
+        byte byteNum = 4;
+        double doubleNum = 20.20d;
+        float floatNum = 3.2f;
+        String name = "Victoria";
+        char letter = 'V';
+        boolean myBool = true;
+
+        //задание 3
+        int a = 10;
+        int b = 22;
+        int c = 40;
+        int d = 2;
+
+        int result = a * (b + (c / d));
+
+        System.out.println(result);
+
+        //задание 4
+        int result2 = c + d;
+
+        if (result2 >= 10 && result2 <= 20) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+        //задание 5
+
+        int result3 = -3;
+
+        if (result3 >= 0) {
+            System.out.println("положительное");
+        } else {
+            System.out.println("отрицательное");
+        }
+
+        //задание 6
+       /* int z = result4(one:3, two:6);
+        if (z < 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+
+        static int result4 ( int one, int two)
+        {
+            int l = f- v;
+            return l;
+        }*/
+
+//задание 7
+System.out.println("Привет, " + name + "!");
+
+    }
+
+
+}
